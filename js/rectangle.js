6ed6e05a1f0c51e48fb61b47b998d12d08ff7968
Jacob <jacobhalleron@protonmail.com>
2021-07-29 00:45:21 +0100

    Started work on a barchart using the canvas API

--- js/rectangle.js
+++ js/rectangle.js
@@ -2,22 +2,50 @@ console.info("rectangle.js is alive")
 
 const rectangle = document.getElementById("theRectangle")
 const ctx = rectangle.getContext("2d")
+let shownArr = []
 
-const testArr = [5, 2, 3, 4, 1, 6, 8, 7]
+let unsortedArr =	[5, 2, 3, 4, 1, 6, 8, 7]
+let sortedArr =	[1, 2, 3, 4, 5, 6, 7, 8]
+let weirdArr =	[8, 8, 1, 3, 5, 0, 2, 4]
+
+const swap = function (array, a, b) {
+	let t = array[a]
+	array[a] = array[b]
+	array[b] = t
+	return array
+}
+
+const getMoves = function (bef, aft) {
+	let moves = []
+	for (i in bef) {
+		moves.push(aft.indexOf(bef[i]))
+	}
+	return moves
+}
 
 const drawSort = function (array) {
-	const barWidth = rectangle.getAttribute("width") / array.length
-	const rectHeight = Number(rectangle.getAttribute("height"))
+	if (shownArr.length == 0) {
+		for (i of array) {shownArr.push(i)}
+	}
+	let moves = getMoves(shownArr, array)
+	for (i in moves) {
+		// if there is a move
+		if (i != moves[i]) {
+			console.log(i, "has moved")
+		}
+	}
+	// draw graph
+	ctx.clearRect(0, 0, rectangle.width, rectangle.height) // clear canvas
+	let barWidth = rectangle.getAttribute("width") / array.length
+	let rectHeight = Number(rectangle.getAttribute("height"))
 	for (i in array) {
+		// draw bar
 		let barUnit = rectHeight / Math.max.apply(null, array)
-		console.log(i * barWidth, 0, i * barWidth + barWidth, barUnit * array[i])
-		//if (i == 2) {
 		ctx.fillStyle = "red"
 		ctx.fillRect(i * barWidth, 0, barWidth, barUnit * array[i])
-		//}
 	}
+	shownArr = array
 }
-drawSort(testArr)
-
-//ctx.fillStyle = "green"
-//ctx.fillRect(20, 20, 100, 200)
+drawSort(unsortedArr)
+swap(unsortedArr, 3, 5)
+drawSort(unsortedArr)
